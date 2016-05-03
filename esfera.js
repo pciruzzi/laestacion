48c9ef39c91620c053913eca02330219d813151e
Santiago <fernandezsantid@gmail.com>
2016-05-03 18:13:40 -0300

    Creadas las clases vertice y esfera, ademas estan las funciones para obtener los buffers de un conjunto de vertices

---
+++ esfera.js
@@ -0,0 +1,145 @@
+function TexturedSphere(latitude_bands, longitude_bands, color){
+
+    this.latitudeBands = latitude_bands;
+    this.longitudeBands = longitude_bands;
+    this.color = color;
+
+    this.tangent = [0,0,0];
+    this.vertex_buffer = null;
+    this.index_buffer = null;
+
+    this.webgl_position_buffer = null;
+    this.webgl_normal_buffer = null;
+    this.webgl_texture_coord_buffer = null;
+    this.webgl_index_buffer = null;
+
+    this.texture = null;
+
+    this.initTexture = function(texture_file){
+        
+        var aux_texture = gl.createTexture();
+        this.texture = aux_texture;
+        this.texture.image = new Image();
+
+        this.texture.image.onload = function () {
+               handleLoadedTexture()
+        }
+        this.texture.image.src = texture_file;
+    }
+
+
+    // Se generan los vertices para la esfera, calculando los datos para una esfera de radio 1
+    // Y también la información de las normales y coordenadas de textura para cada vertice de la esfera
+    // La esfera se renderizara utilizando triangulos, para ello se arma un buffer de índices 
+    // a todos los triángulos de la esfera
+    this.initBuffers = function(){
+
+        this.vertex_buffer = [];
+
+        var latNumber;
+        var longNumber;
+
+        for (latNumber=0; latNumber <= this.latitudeBands; latNumber++) {
+            var theta = latNumber * Math.PI / this.latitudeBands;
+            var sinTheta = Math.sin(theta);
+            var cosTheta = Math.cos(theta);
+
+            for (longNumber=0; longNumber <= this.longitudeBands; longNumber++) {
+                var phi = longNumber * 2 * Math.PI / this.longitudeBands;
+                var sinPhi = Math.sin(phi);
+                var cosPhi = Math.cos(phi);
+
+                var x = cosPhi * sinTheta;
+                var y = cosTheta;
+                var z = sinPhi * sinTheta;
+                var u = 1.0 - (longNumber / this.longitudeBands);
+                var v = 1.0 - (latNumber / this.latitudeBands);
+
+                var position = [x,y,z];
+                var normal = [x,y,z];
+                var texture = [u,v];
+
+                var verticeActual = new vertice(position, this.color, normal, this.tangent, texture);
+                this.vertex_buffer.push(verticeActual);
+            }
+        }
+
+        // Buffer de indices de los triangulos
+        this.index_buffer = [];
+      
+        for (latNumber=0; latNumber < this.latitudeBands; latNumber++) {
+            for (longNumber=0; longNumber < this.longitudeBands; longNumber++) {
+                var first = (latNumber * (this.longitudeBands + 1)) + longNumber;
+                var second = first + this.longitudeBands + 1;
+                this.index_buffer.push(first);
+                this.index_buffer.push(second);
+                this.index_buffer.push(first + 1);
+
+                this.index_buffer.push(second);
+                this.index_buffer.push(second + 1);
+                this.index_buffer.push(first + 1);
+            }
+        }
+
+        // Creación e Inicialización de los buffers a nivel de OpenGL
+        var position_buffer = getPositionBuffer(this.vertex_buffer);
+        var normal_buffer = getNormalBuffer(this.vertex_buffer);
+        var texture_coord_buffer = getTextureBuffer(this.vertex_buffer);
+
+        this.webgl_normal_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal_buffer), gl.STATIC_DRAW);
+        this.webgl_normal_buffer.itemSize = 3;
+        this.webgl_normal_buffer.numItems = normal_buffer.length / 3;
+
+        this.webgl_texture_coord_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coord_buffer), gl.STATIC_DRAW);
+        this.webgl_texture_coord_buffer.itemSize = 2;
+        this.webgl_texture_coord_buffer.numItems = texture_coord_buffer.length / 2;
+
+        this.webgl_position_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position_buffer), gl.STATIC_DRAW);
+        this.webgl_position_buffer.itemSize = 3;
+        this.webgl_position_buffer.numItems = position_buffer.length / 3;
+
+        this.webgl_index_buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
+        this.webgl_index_buffer.itemSize = 1;
+        this.webgl_index_buffer.numItems = this.index_buffer.length;
+    }
+
+    this.draw = function(modelMatrix){
+
+        // Se configuran los buffers que alimentarán el pipeline
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+        gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_texture_coord_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, this.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+        gl.activeTexture(gl.TEXTURE0);
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        gl.uniform1i(shaderProgram.samplerUniform, 0);
+
+        gl.uniformMatrix4fv(shaderProgram.ModelMatrixUniform, false, modelMatrix);
+        var normalMatrix = mat3.create();
+        mat3.fromMat4(normalMatrix, modelMatrix);
+        mat3.invert(normalMatrix, normalMatrix);
+        mat3.transpose(normalMatrix, normalMatrix);
+        gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
+
+        gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
+        //gl.drawElements(gl.LINE_LOOP, this.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, this.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
+        /////////////////////////////////
+    }
+
+}
