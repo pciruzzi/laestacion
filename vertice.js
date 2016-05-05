1278473760c3d1f101d273232d95edf9ccc89a9c
Pablo <p.ciruzzi@hotmail.com>
2016-05-05 09:51:17 -0300

    Cambio tabs por espacios para hacer todo uniforme. Agrego banco de pruebas: en la funcion webGLStart poner el objeto que se quiere usar y listo. Ahora tiene un escalado de 4 para que se vea mejor, pero se puede cambiar

--- vertice.js
+++ vertice.js
@@ -1,19 +1,19 @@
 function vertice(posicion, color, normal, tangente, coordTextura) {
-	this.posicion = posicion;
-	this.color = color;
-	this.normal = normal;
-	this.tangente = tangente;
-	this.coordTextura = coordTextura;
-	
-	var tx = this.tangente[0];
-	var ty = this.tangente[1];
-	var tz = this.tangente[2];
-	var nx = this.normal[0];
-	var ny = this.normal[1];
-	var nz = this.normal[2];
+    this.posicion = posicion;
+    this.color = color;
+    this.normal = normal;
+    this.tangente = tangente;
+    this.coordTextura = coordTextura;
+    
+    var tx = this.tangente[0];
+    var ty = this.tangente[1];
+    var tz = this.tangente[2];
+    var nx = this.normal[0];
+    var ny = this.normal[1];
+    var nz = this.normal[2];
 
-	this.binormal = [];
-	this.binormal[0] = ((ty * nz) - (ny * tz));
-	this.binormal[1] = ((tx * nz) - (nx * tz));
-	this.binormal[2] = ((tx * ny) - (nx * ty));
+    this.binormal = [];
+    this.binormal[0] = ((ty * nz) - (ny * tz));
+    this.binormal[1] = ((tx * nz) - (nx * tz));
+    this.binormal[2] = ((tx * ny) - (nx * ty));
 }
