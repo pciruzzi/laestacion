02edc160c4963679ef04d54121b12365f955b111
Santiago <fernandezsantid@gmail.com>
2016-05-07 18:47:14 -0300

    Hecho lo que las figuras se dibujen con un color especifico, sigo con el la implementacion del cubo

--- cilindro.js
+++ cilindro.js
@@ -1,8 +1,9 @@
-function Cilindro(latitude_bands, longitude_bands, color){
+function Cilindro(latitude_bands, longitude_bands, color, esTexturada){
 
     this.latitudeBands = latitude_bands;
     this.longitudeBands = longitude_bands;
     this.color = color;
+    this.esTexturada = esTexturada;
 
     this.tangent = [0,0,0]; // No interesa la tangente en este caso
     this.vertex_buffer = null;
@@ -11,7 +12,7 @@ function Cilindro(latitude_bands, longitude_bands, color){
     this.webgl_position_buffer = null;
     this.webgl_normal_buffer = null;
     this.webgl_texture_coord_buffer = null;
-    //this.webgl_color_buffer = null;
+    this.webgl_color_buffer = null;
     this.webgl_index_buffer = null;
 
     this.texture = null;
@@ -30,10 +31,10 @@ function Cilindro(latitude_bands, longitude_bands, color){
 
 
     // Se generan los vertices para el cilindro, calculando los datos para un cilindro de radio 1 y altura 1.
-    // El cilindro es alrededor del eje z
-    // Y también la información de las normales y coordenadas de textura para cada vertice del cilindro
-    // La esfera se renderizara utilizando triangle_strip, para ello se arma un buffer de índices 
-    // a todos los vértices del cilindro
+    // El cilindro es alrededor del eje z.
+    // Y también la información de las normales y coordenadas de textura o color para cada vertice del cilindro.
+    // El cilindro se renderizara utilizando triangle_strip, para ello se arma un buffer de índices 
+    // a todos los vértices del cilindro.
     this.initBuffers = function(){
 
         this.vertex_buffer = [];
@@ -54,12 +55,16 @@ function Cilindro(latitude_bands, longitude_bands, color){
                 var x = cosTheta;
                 var y = sinTheta;
                 var z = altura;
-                var u = 1.0 - (longNumber / this.longitudeBands);
-                var v = 1.0 - (latNumber / this.latitudeBands);
 
                 var position = [x,y,z];
                 var normal = [x,y,0];
-                var texture = [u,v];
+                var texture = [0,0];
+
+                if (this.esTexturada) {
+                    var u = 1.0 - (longNumber / this.longitudeBands);
+                    var v = 1.0 - (latNumber / this.latitudeBands);
+                    texture = [u,v];
+                }
 
                 var verticeActual = new vertice(position, this.color, normal, this.tangent, texture);
                 this.vertex_buffer.push(verticeActual);
@@ -75,8 +80,6 @@ function Cilindro(latitude_bands, longitude_bands, color){
         // Creación e Inicialización de los buffers a nivel de OpenGL
         var position_buffer = getPositionBuffer(this.vertex_buffer);
         var normal_buffer = getNormalBuffer(this.vertex_buffer);
-        var texture_coord_buffer = getTextureBuffer(this.vertex_buffer);
-        //var color_buffer = getColorBuffer(this.vertex_buffer);
 
         this.webgl_normal_buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
@@ -84,18 +87,6 @@ function Cilindro(latitude_bands, longitude_bands, color){
         this.webgl_normal_buffer.itemSize = 3;
         this.webgl_normal_buffer.numItems = normal_buffer.length / 3;
 
-        this.webgl_texture_coord_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coord_buffer), gl.STATIC_DRAW);
-        this.webgl_texture_coord_buffer.itemSize = 2;
-        this.webgl_texture_coord_buffer.numItems = texture_coord_buffer.length / 2;
-
-/*        this.webgl_color_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color_buffer), gl.STATIC_DRAW);
-        this.webgl_color_buffer.itemSize = 3;
-        this.webgl_color_buffer.numItems = color_buffer.length / 3;
-*/
         this.webgl_position_buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(position_buffer), gl.STATIC_DRAW);
@@ -107,6 +98,24 @@ function Cilindro(latitude_bands, longitude_bands, color){
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
         this.webgl_index_buffer.itemSize = 1;
         this.webgl_index_buffer.numItems = this.index_buffer.length;
+
+        if (this.esTexturada) {
+            var texture_coord_buffer = getTextureBuffer(this.vertex_buffer);
+
+            this.webgl_texture_coord_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coord_buffer), gl.STATIC_DRAW);
+            this.webgl_texture_coord_buffer.itemSize = 2;
+            this.webgl_texture_coord_buffer.numItems = texture_coord_buffer.length / 2;
+        } else {
+            var color_buffer = getColorBuffer(this.vertex_buffer);
+
+            this.webgl_color_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color_buffer), gl.STATIC_DRAW);
+            this.webgl_color_buffer.itemSize = 3;
+            this.webgl_color_buffer.numItems = color_buffer.length / 3;
+        }
     }
 
     this.draw = function(modelMatrix){
@@ -114,15 +123,21 @@ function Cilindro(latitude_bands, longitude_bands, color){
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
-        gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_texture_coord_buffer.itemSize, gl.FLOAT, false, 0, 0);
-
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
         gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, this.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.uniform1i(shaderProgram.samplerUniform, 0);
+        if (this.esTexturada) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_texture_coord_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+            gl.uniform1i(shaderProgram.samplerUniform, 0);
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        } else {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_color_buffer.itemSize, gl.FLOAT, false, 0, 0);
+        }
 
         gl.uniformMatrix4fv(shaderProgram.ModelMatrixUniform, false, modelMatrix);
         var normalMatrix = mat3.create();
@@ -130,8 +145,6 @@ function Cilindro(latitude_bands, longitude_bands, color){
         mat3.invert(normalMatrix, normalMatrix);
         mat3.transpose(normalMatrix, normalMatrix);
         gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
-
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
         
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
         //gl.drawElements(gl.LINE_LOOP, this.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
@@ -143,15 +156,19 @@ function Cilindro(latitude_bands, longitude_bands, color){
             var x = 0;
             var y = 0;
             var z = altura;
-            var u = 1.0 - (longNumber / this.longitudeBands);
-            var v = 1.0 - (altura);
 
             var position = [x,y,z];
             var zNormal;
             if (altura == 0) zNormal = -1;
             else zNormal = 1;
             var normal = [x,y,zNormal];
-            var texture = [u,v];
+            var texture = [0,0];
+
+            if (this.esTexturada) {
+                var u = 1.0 - (longNumber / this.longitudeBands);
+                var v = 1.0 - (altura);
+                texture = [u,v];
+            }
 
             var verticeActual = new vertice(position, this.color, normal, this.tangent, texture);
             this.vertex_buffer.push(verticeActual);
--- esfera.js
+++ esfera.js
@@ -1,8 +1,9 @@
-function Esfera(latitude_bands, longitude_bands, color){
+function Esfera(latitude_bands, longitude_bands, color, esTexturada){
 
     this.latitudeBands = latitude_bands;
     this.longitudeBands = longitude_bands;
     this.color = color;
+    this.esTexturada = esTexturada;
 
     this.tangent = [0,0,0]; // No interesa la tangente en este caso
     this.vertex_buffer = null;
@@ -11,6 +12,7 @@ function Esfera(latitude_bands, longitude_bands, color){
     this.webgl_position_buffer = null;
     this.webgl_normal_buffer = null;
     this.webgl_texture_coord_buffer = null;
+    this.webgl_color_buffer = null;
     this.webgl_index_buffer = null;
 
     this.texture = null;
@@ -28,10 +30,10 @@ function Esfera(latitude_bands, longitude_bands, color){
     }
 
 
-    // Se generan los vertices para la esfera, calculando los datos para una esfera de radio 1
-    // Y también la información de las normales y coordenadas de textura para cada vertice de la esfera
+    // Se generan los vertices para la esfera, calculando los datos para una esfera de radio 1.
+    // Y también la información de las normales y coordenadas de textura para cada vertice de la esfera.
     // La esfera se renderizara utilizando triangle_strip, para ello se arma un buffer de índices 
-    // de todos los vértices de la esfera
+    // de todos los vértices de la esfera.
     this.initBuffers = function(){
 
         this.vertex_buffer = [];
@@ -52,12 +54,16 @@ function Esfera(latitude_bands, longitude_bands, color){
                 var x = cosPhi * sinTheta;
                 var y = cosTheta;
                 var z = sinPhi * sinTheta;
-                var u = 1.0 - (longNumber / this.longitudeBands);
-                var v = 1.0 - (latNumber / this.latitudeBands);
-
+                
                 var position = [x,y,z];
                 var normal = [x,y,z];
-                var texture = [u,v];
+                var texture = [0,0];
+
+                if (this.esTexturada) {
+                    var u = 1.0 - (longNumber / this.longitudeBands);
+                    var v = 1.0 - (latNumber / this.latitudeBands);
+                    texture = [u,v];
+                }                
 
                 var verticeActual = new vertice(position, this.color, normal, this.tangent, texture);
                 this.vertex_buffer.push(verticeActual);
@@ -70,19 +76,12 @@ function Esfera(latitude_bands, longitude_bands, color){
         // Creación e Inicialización de los buffers a nivel de OpenGL
         var position_buffer = getPositionBuffer(this.vertex_buffer);
         var normal_buffer = getNormalBuffer(this.vertex_buffer);
-        var texture_coord_buffer = getTextureBuffer(this.vertex_buffer);
 
         this.webgl_normal_buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
         gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normal_buffer), gl.STATIC_DRAW);
         this.webgl_normal_buffer.itemSize = 3;
         this.webgl_normal_buffer.numItems = normal_buffer.length / 3;
-
-        this.webgl_texture_coord_buffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coord_buffer), gl.STATIC_DRAW);
-        this.webgl_texture_coord_buffer.itemSize = 2;
-        this.webgl_texture_coord_buffer.numItems = texture_coord_buffer.length / 2;
 
         this.webgl_position_buffer = gl.createBuffer();
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
@@ -95,6 +94,24 @@ function Esfera(latitude_bands, longitude_bands, color){
         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(this.index_buffer), gl.STATIC_DRAW);
         this.webgl_index_buffer.itemSize = 1;
         this.webgl_index_buffer.numItems = this.index_buffer.length;
+
+        if (this.esTexturada) {
+            var texture_coord_buffer = getTextureBuffer(this.vertex_buffer);
+
+            this.webgl_texture_coord_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coord_buffer), gl.STATIC_DRAW);
+            this.webgl_texture_coord_buffer.itemSize = 2;
+            this.webgl_texture_coord_buffer.numItems = texture_coord_buffer.length / 2;
+        } else {
+            var color_buffer = getColorBuffer(this.vertex_buffer);
+
+            this.webgl_color_buffer = gl.createBuffer();
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+            gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(color_buffer), gl.STATIC_DRAW);
+            this.webgl_color_buffer.itemSize = 3;
+            this.webgl_color_buffer.numItems = color_buffer.length / 3;
+        }
     }
 
     this.draw = function(modelMatrix){
@@ -103,15 +120,21 @@ function Esfera(latitude_bands, longitude_bands, color){
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_position_buffer);
         gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.webgl_position_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
-        gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_texture_coord_buffer.itemSize, gl.FLOAT, false, 0, 0);
-
         gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_normal_buffer);
         gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute, this.webgl_normal_buffer.itemSize, gl.FLOAT, false, 0, 0);
 
-        gl.activeTexture(gl.TEXTURE0);
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
-        gl.uniform1i(shaderProgram.samplerUniform, 0);
+        if (this.esTexturada) {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_texture_coord_buffer);
+            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_texture_coord_buffer.itemSize, gl.FLOAT, false, 0, 0);
+
+            gl.activeTexture(gl.TEXTURE0);
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+            gl.uniform1i(shaderProgram.samplerUniform, 0);
+            gl.bindTexture(gl.TEXTURE_2D, this.texture);
+        } else {
+            gl.bindBuffer(gl.ARRAY_BUFFER, this.webgl_color_buffer);
+            gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, this.webgl_color_buffer.itemSize, gl.FLOAT, false, 0, 0);
+        }
 
         gl.uniformMatrix4fv(shaderProgram.ModelMatrixUniform, false, modelMatrix);
         var normalMatrix = mat3.create();
@@ -119,8 +142,6 @@ function Esfera(latitude_bands, longitude_bands, color){
         mat3.invert(normalMatrix, normalMatrix);
         mat3.transpose(normalMatrix, normalMatrix);
         gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);
-
-        gl.bindTexture(gl.TEXTURE_2D, this.texture);
         
         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.webgl_index_buffer);
         //gl.drawElements(gl.LINE_LOOP, this.webgl_index_buffer.numItems, gl.UNSIGNED_SHORT, 0);
