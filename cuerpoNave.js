45358b825451a50381f168b05ac864ef3a48dd9c
Pablo <p.ciruzzi@hotmail.com>
2016-05-26 12:59:23 -0300

    Le agrego movimiento a las patas de la nave (Ahora son cilindros) con la letra x

--- cuerpoNave.js
+++ cuerpoNave.js
@@ -1,6 +1,10 @@
 function CuerpoNave() {
     this.parte1 = null;
 
+    this.pata1 = null;
+    this.pata2 = null;
+    this.pata3 = null;
+
     this.punto1 = [0,0,8];
     this.punto2 = [8,0,8];
     this.punto3 = [9,-10,9];
@@ -14,6 +18,13 @@ function CuerpoNave() {
         this.parte1 = new Cubo(10.0, 17.0, 30.0, getColor("red"), false);
         //this.parte1 = new Paralelepipedo(this.punto1, this.punto2, this.punto3, this.punto4, this.punto5, this.punto6, this.punto7, this.punto8, getColor("red"), false);
         this.parte1.initBuffers();
+
+        this.pata1 = new Cilindro(4, 20, getColor("blue"), false);
+        this.pata1.initBuffers();
+        this.pata2 = new Cilindro(4, 20, getColor("blue"), false);
+        this.pata2.initBuffers();
+        this.pata3 = new Cilindro(4, 20, getColor("blue"), false);
+        this.pata3.initBuffers();
     }
 
     this.draw = function(modelMatrix) {
@@ -23,5 +34,26 @@ function CuerpoNave() {
         mat4.multiply(model_matrix_cuerpo, model_matrix_cuerpo, modelMatrix);
 
         this.parte1.draw(model_matrix_cuerpo);
+
+        var model_matrix_pata1 = mat4.create();
+        mat4.identity(model_matrix_pata1);
+        mat4.translate(model_matrix_pata1, model_matrix_cuerpo, [-5,traslacionPatasNave,-10]);
+        mat4.rotate(model_matrix_pata1, model_matrix_pata1, Math.PI/2, [1,0,0]);
+        mat4.scale(model_matrix_pata1, model_matrix_pata1, [1,1,5]);
+        this.pata1.draw(model_matrix_pata1);
+
+        var model_matrix_pata2 = mat4.create();
+        mat4.identity(model_matrix_pata2);
+        mat4.translate(model_matrix_pata2, model_matrix_cuerpo, [5,traslacionPatasNave,-10]);
+        mat4.rotate(model_matrix_pata2, model_matrix_pata2, Math.PI/2, [1,0,0]);
+        mat4.scale(model_matrix_pata2, model_matrix_pata2, [1,1,5]);
+        this.pata2.draw(model_matrix_pata2);
+
+        var model_matrix_pata3 = mat4.create();
+        mat4.identity(model_matrix_pata3);
+        mat4.translate(model_matrix_pata3, model_matrix_cuerpo, [0,traslacionPatasNave,10]);
+        mat4.rotate(model_matrix_pata3, model_matrix_pata3, Math.PI/2, [1,0,0]);
+        mat4.scale(model_matrix_pata3, model_matrix_pata3, [1,1,5]);
+        this.pata3.draw(model_matrix_pata3);
     }
 }
--- keyboardhandle.js
+++ keyboardhandle.js
@@ -6,112 +6,117 @@ onkeydown = function(evento){
     //Seleccion de camara
     // Numero 1, pone la camara global (aerea).
     if ((teclaAscii == '97') || (teclaAscii == '49')) {
-       camaraGlobal    		= true;
-       camaraCabina			  = false;
-       camaraPersecucion 	= false;
-       camaraPersona  		= false;
-       rotarCamaraY 		  = 0.0;
-       rotarCamaraX 		  = 0.0;
+        camaraGlobal    	= true;
+        camaraCabina		= false;
+        camaraPersecucion   = false;
+        camaraPersona  		= false;
+        rotarCamaraY 		= 0.0;
+        rotarCamaraX 		= 0.0;
     }
     // Numero 2, pone la camara en la cabina de la nave.
     else if ((teclaAscii == '98') || (teclaAscii == '50')) {
-       camaraGlobal    		= false;
-       camaraCabina   		= true;
-       camaraPersecucion  = false;
-       camaraPersona  		= false;
-       rotarCamaraY 		  = 0.0;
-       rotarCamaraX 		  = 0.0;
+        camaraGlobal    	= false;
+        camaraCabina   		= true;
+        camaraPersecucion   = false;
+        camaraPersona  		= false;
+        rotarCamaraY 		= 0.0;
+        rotarCamaraX 		= 0.0;
     }
     // Numero 3, pone la camara detras de la nava, a modo de perseucion.
     else if ((teclaAscii == '99') || (teclaAscii == '51')) {
-       camaraGlobal    		= false;
-       camaraCabina   		= false;
-       camaraPersecucion 	= true;
-       camaraPersona  		= false;
-       rotarCamaraY 		  = 0.0;
-       rotarCamaraX 		  = 0.0;
+        camaraGlobal    	= false;
+        camaraCabina   		= false;
+        camaraPersecucion 	= true;
+        camaraPersona  		= false;
+        rotarCamaraY 		= 0.0;
+        rotarCamaraX 		= 0.0;
     }
     // Numero 4, pone la camara en la persona en la bahia de carga.
     else if ((teclaAscii == '100') || (teclaAscii == '52')) {
-       camaraGlobal    		= false;
-       camaraCabina   		= false;
-       camaraPersecucion 	= false;
-       camaraPersona  		= true;
-       rotarCamaraY 		  = 0.0;
-       rotarCamaraX 		  = 0.0;
+        camaraGlobal    	= false;
+        camaraCabina   		= false;
+        camaraPersecucion 	= false;
+        camaraPersona  		= true;
+        rotarCamaraY 		= 0.0;
+        rotarCamaraX 		= 0.0;
     }
 
     //Manejo del Zoom
     // Zoom '+'
     else if ((teclaAscii == '107') || (teclaAscii == '171')) {
-       aumento = aumento + 4;
+        aumento = aumento + 4;
     }
     // Zoom '-'
     else if ((teclaAscii == '173') || (teclaAscii == '109')) {
-       aumento = aumento - 4;
+        aumento = aumento - 4;
     }
 
     //Teclas para el pliegue y despligue de la antena
     if (tecla == "p" || tecla == "P") {
-      plegarODesplegarAntena = true;
+        plegarODesplegarAntena = true;
+    }
+
+    //Teclas de pliegue y despliegue de las patas de la nave
+    if (tecla == "x" || tecla == "X") {
+        plegarODesplegarPatas = true;
     }
 
     //Estas solo sirven para mover la persona en la bahia de carga.
     //Se puede apreciar mejor cuando la camara esta en la bahia de carga.
     // Muevo persona para adelante
     if((tecla == "w") || (tecla == "W")){
-      traslacionPersonaZ = traslacionPersonaZ - Math.cos(-degToRad(rotarCamaraY));   
-      traslacionPersonaX = traslacionPersonaX - Math.sin(-degToRad(rotarCamaraY));
+        traslacionPersonaZ = traslacionPersonaZ - Math.cos(-degToRad(rotarCamaraY));   
+        traslacionPersonaX = traslacionPersonaX - Math.sin(-degToRad(rotarCamaraY));
     }
     // Muevo persona para atras
     if((tecla == "s") || (tecla == "S")){
-      traslacionPersonaZ = traslacionPersonaZ + Math.cos(-degToRad(rotarCamaraY));   
-      traslacionPersonaX = traslacionPersonaX + Math.sin(-degToRad(rotarCamaraY));
+        traslacionPersonaZ = traslacionPersonaZ + Math.cos(-degToRad(rotarCamaraY));   
+        traslacionPersonaX = traslacionPersonaX + Math.sin(-degToRad(rotarCamaraY));
     }
 
     // Muevo persona para el costado derecho
     if((tecla == "d") || (tecla == "D")){
-      traslacionPersonaZ = traslacionPersonaZ + Math.sin(degToRad(rotarCamaraY));   
-      traslacionPersonaX = traslacionPersonaX + Math.cos(degToRad(rotarCamaraY));
+        traslacionPersonaZ = traslacionPersonaZ + Math.sin(degToRad(rotarCamaraY));   
+        traslacionPersonaX = traslacionPersonaX + Math.cos(degToRad(rotarCamaraY));
     }
     // Muevo persona para el costado izquierda
     if((tecla == "a") || (tecla == "A")){
-      traslacionPersonaZ = traslacionPersonaZ - Math.sin(degToRad(rotarCamaraY)); 
-      traslacionPersonaX = traslacionPersonaX - Math.cos(degToRad(rotarCamaraY));
+        traslacionPersonaZ = traslacionPersonaZ - Math.sin(degToRad(rotarCamaraY)); 
+        traslacionPersonaX = traslacionPersonaX - Math.cos(degToRad(rotarCamaraY));
     }
 
     //Estas teclas van a servir para el manejo de la nave
     //Muevo la nave para arriba
     if((tecla == "i") || (tecla == "I")){
-      nave.onTeclaDown(nave.TECLA_ARRIBA);
+        nave.onTeclaDown(nave.TECLA_ARRIBA);
     }
     //Muevo la nave para abajo
     if((tecla == "k") || (tecla == "K")){
-      nave.onTeclaDown(nave.TECLA_ABAJO);
+        nave.onTeclaDown(nave.TECLA_ABAJO);
     }
     //Muevo la nave para la izquierda
     if((tecla == "j") || (tecla == "J")){
-      nave.onTeclaDown(nave.TECLA_IZQUIERDA);
+        nave.onTeclaDown(nave.TECLA_IZQUIERDA);
     }
     //Muevo la nave para la derecha
     if((tecla == "l") || (tecla == "L")){
-      nave.onTeclaDown(nave.TECLA_DERECHA);
+        nave.onTeclaDown(nave.TECLA_DERECHA);
     }
     //Acelero la nave
     if((tecla == "m") || (tecla == "M")){
-      nave.onTeclaDown(nave.TECLA_MAS);
+        nave.onTeclaDown(nave.TECLA_MAS);
     }
     //Desacelero la nave
     if((tecla == "n") || (tecla == "N")){
-      nave.onTeclaDown(nave.TECLA_MENOS);
+        nave.onTeclaDown(nave.TECLA_MENOS);
     }
     //Muevo la nave para en sentido horario
     if((tecla == "o") || (tecla == "O")){
-      nave.onTeclaDown(nave.TECLA_GIRO_HORARIO);
+        nave.onTeclaDown(nave.TECLA_GIRO_HORARIO);
     }
     //Muevo la nave para en sentido antihorario
     if((tecla == "u") || (tecla == "U")){
-      nave.onTeclaDown(nave.TECLA_GIRO_ANTIHORARIO);
+        nave.onTeclaDown(nave.TECLA_GIRO_ANTIHORARIO);
     }
 }
 
@@ -122,34 +127,34 @@ onkeyup = function(evento){
     //Estas teclas van a servir para el manejo de la nave
     //Dejo de mover la nave para arriba
     if((tecla == "i") || (tecla == "I")){
-      nave.onTeclaUp(nave.TECLA_ARRIBA);
+        nave.onTeclaUp(nave.TECLA_ARRIBA);
     }
     //Dejo de mover la nave para abajo
     if((tecla == "k") || (tecla == "K")){
-      nave.onTeclaUp(nave.TECLA_ABAJO);
+        nave.onTeclaUp(nave.TECLA_ABAJO);
     }
     //Dejo de mover la nave para la izquierda
     if((tecla == "j") || (tecla == "J")){
-      nave.onTeclaUp(nave.TECLA_IZQUIERDA);
+        nave.onTeclaUp(nave.TECLA_IZQUIERDA);
     }
     //Dejo de mover la nave para la derecha
     if((tecla == "l") || (tecla == "L")){
-      nave.onTeclaUp(nave.TECLA_DERECHA);
+        nave.onTeclaUp(nave.TECLA_DERECHA);
     }
     //Dejo de acelerar la nave
     if((tecla == "m") || (tecla == "M")){
-      nave.onTeclaUp(nave.TECLA_MAS);
+        nave.onTeclaUp(nave.TECLA_MAS);
     }
     //Dejo de desacelerar la nave
     if((tecla == "n") || (tecla == "N")){
-      nave.onTeclaUp(nave.TECLA_MENOS);
+        nave.onTeclaUp(nave.TECLA_MENOS);
     }
     //Dejo de mover la nave para en sentido horario
     if((tecla == "o") || (tecla == "O")){
-      nave.onTeclaUp(nave.TECLA_GIRO_HORARIO);
+        nave.onTeclaUp(nave.TECLA_GIRO_HORARIO);
     }
     //Dejo de mover la nave para en sentido antihorario
     if((tecla == "u") || (tecla == "U")){
-      nave.onTeclaUp(nave.TECLA_GIRO_ANTIHORARIO);
+        nave.onTeclaUp(nave.TECLA_GIRO_ANTIHORARIO);
     }
 }
